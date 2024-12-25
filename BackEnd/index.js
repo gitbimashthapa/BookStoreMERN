0d2300c0ffc860159c45bad8bb34f480df538097
gitbimashthapa <mailbimashthapa@gmail.com>
2024-12-25 22:59:54 +0545

    commit

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,24 +11,10 @@ return response.status(234).send('Welcome');
 
 
 // Route for save a new book
-app.post('/books', async (request, response) => {
-    try{
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-message: 'send all required fields: title, author, publishYear',
-            });
-                }
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: 'Error in Saving Book'});
-    }
-})
+app.post('/books')
 
-mongoose.connect(mongoDBURL)
+mongoose
+.connect(mongoDBURL)
 .then(() => {
     console.log('App Connected to database');
     app.listen(PORT, () => {
